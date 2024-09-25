085eb368fc69607b6610718b40e9af43c397f96a
alex2702 <16482004+alex2702@users.noreply.github.com>
2024-09-25 23:38:57 +0200

    Add redis

--- spendings/settings/production.py
+++ spendings/settings/production.py
@@ -19,3 +19,16 @@ DATABASES = {
 		"PORT": "5432",
 	}
 }
+
+CACHES = {
+	"default": {
+		"BACKEND": "django_redis.cache.RedisCache",
+		"LOCATION": "redis://127.0.0.1:6379/0",
+		"OPTIONS": {
+			"CLIENT_CLASS": "django_redis.client.DefaultClient",
+		},
+	}
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
--- spendings/settings/testing.py
+++ spendings/settings/testing.py
@@ -15,3 +15,9 @@ DATABASES = {
 		"NAME": BASE_DIR / "db.sqlite3",
 	}
 }
+
+CACHES = {
+	"default": {
+		"BACKEND": "django.core.cache.backends.dummy.DummyCache",
+	}
+}
